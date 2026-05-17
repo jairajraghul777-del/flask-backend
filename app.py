80ede0c55427acfce6dd08c3293981eb6c9a9652
jairajraghul777-del <jairajraghul777@gmail.com>
2026-05-17 11:12:17 +0530

    signuppage

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
-from flask import Flask
+ from flask import Flask, request, jsonify
+import mysql.connector
 
 app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="900billion@yuvraj1",
+    database="myapp"
+)
+
+cursor = db.cursor()
 
 @app.route('/')
 def home():
     return "Backend Running ✔️"
 
-if __name__ == "__main__":
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+
+    name = data.get('name')
+    password = data.get('password')
+
+    # Empty check
+    if not name or not password:
+        return jsonify({
+            "success": False,
+            "message": "Name and Password required"
+        }), 400
+
+    # Insert into table
+    query = "INSERT INTO users (name, password) VALUES (%s, %s)"
+    values = (name, password)
+
+    cursor.execute(query, values)
+    db.commit()
+
+    return jsonify({
+        "success": True,
+        "message": "Signup Successful"
+    })
+
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=10000)
- 
  
  
  
