979d4413605d2b423e20258e864c16d1c59c4a14
jairajraghul777-del <jairajraghul777@gmail.com>
2026-05-17 11:17:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 import mysql.connector
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="900billion@yuvraj1",
+    password="900billion@yuvraj1s",
     database="myapp"
 )
 
