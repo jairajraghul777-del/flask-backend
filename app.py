8812bdb23b4be0ff1640c53f42cd20071bbe97d1
jairajraghul777-del <jairajraghul777@gmail.com>
2026-05-17 10:46:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,57 +1,13 @@
-from flask import Flask, request, jsonify
-import mysql.connector
+from flask import Flask
 
 app = Flask(__name__)
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="900billion@yuvraj1",
-    database="myapp"
-)
+@app.route('/')
+def home():
+    return "Backend Running ✔️"
 
-cursor = db.cursor()
-
-
-@app.route('/signup', methods=['POST'])
-def signup():
-
-    data = request.json
-
-    name = data.get("name")
-    password = data.get("password")
-
-    try:
-        sql = "INSERT INTO users (name, password) VALUES (%s, %s)"
-        values = (name, password)
-
-        cursor.execute(sql, values)
-        db.commit()
-
-        return jsonify({
-            "success": True,
-            "message": "User created"
-        })
-
-    except:
-        return jsonify({
-            "success": False,
-            "message": "Password already exists"
-        })
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
- 
-print("Server started")
-print(db)
- 
- 
- 
- 
- 
- 
- 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
  
  
  
