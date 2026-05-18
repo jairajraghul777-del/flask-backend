9be551740b4bf4126327a3f953c21c1d6b127c36
jairajraghul777-del <jairajraghul777@gmail.com>
2026-05-18 11:17:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def home():
 def signup():
     data = request.get_json()
 
-    name = data.get("name")
+    username = data.get("username")
     password = data.get("password")
 
     query = """
@@ -33,17 +33,61 @@ def signup():
     VALUES (%s, %s)
     """
 
-    cursor.execute(query, (name, password))
+    cursor.execute(query, (username, password))
     conn
   
     if __name__ == "__main__":
      app.run(host="0.0.0.0", port=10000)
-   
- 
- 
- 
- 
+  
+from flask import Flask, request, jsonify
+import psycopg2
 
-  
-  
- 
+app = Flask(__name__)
+
+conn = psycopg2.connect(
+    host="aws-1-ap-northeast-1.pooler.supabase.com",
+    port="6543",
+    database="postgres",
+    user="postgres.mymlxkrilepfnxsokctn",
+    password="1000billion@yjm3",
+    sslmode="require"
+)
+
+cursor = conn.cursor()
+
+
+@app.route('/add-card', methods=['POST'])
+def add_card():
+    data = request.json
+
+    name = data.get('name')
+    bio = data.get('bio')
+    skills = data.get('skills')
+    image_url = data.get('image_url')
+
+    cursor.execute("""
+        INSERT INTO id_cards
+        (name, bio, skills, image_url)
+        VALUES (%s, %s, %s, %s)
+    """, (name, bio, skills, image_url))
+
+    conn.commit()
+
+    return jsonify({"message": "Card saved"})
+
+
+@app.route('/')
+def home():
+    return "Server Running"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
